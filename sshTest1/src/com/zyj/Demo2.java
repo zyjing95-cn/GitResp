9813bb734c7daa5eddfb91e5dbef86ad7c7aca75
zyj <724272576@qq.com>
2021-02-25 14:57:13 +0800

    更新方法

--- sshTest1/src/com/zyj/Demo2.java
+++ sshTest1/src/com/zyj/Demo2.java
@@ -3,6 +3,7 @@ package com.zyj;
 public class Demo2 {
     public static void secondMethod(){
         System.out.println("Demo2第一个方法");
-        System.out.println("Demo2第二个方法");
+        System.out.println("第一次更新该方法");
+        System.out.println("第二次更新该方法");
     }
 }
